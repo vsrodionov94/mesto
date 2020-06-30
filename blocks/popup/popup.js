578fceae708de3a5c35b017b5ff92cbb5bb2af3e
Vladimir Rodionov <vsrodionov94@yandex.ru>
2020-06-30 19:51:30 +0700

    основные фиксы готовы

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,8 +1,12 @@
-// Открываем и закрываем окна
 let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.edit-button');
+let editButton = profile.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let escButton = popup.querySelector('.esc-button');
+let escButton = popup.querySelector('.popup__esc-button');
+let name = profile.querySelector('.profile__name-text');
+let proffesion = profile.querySelector('.profile__proffession');
+let fieldName = popup.querySelector('.popup__field_assign_name');
+let fieldProffesion = popup.querySelector('.popup__field_assign_proffesion');
+let submitButtonProfile = popup.querySelector('.popup__submit-button');
 
 function openEditProfile() {
   popup.classList.add('popup_opened');
@@ -12,23 +16,14 @@ function closeEditProfile() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openEditProfile);
-
-escButton.addEventListener('click', closeEditProfile);
-
-// Редактируем поля
-
-let submitButtonProfile = popup.querySelector('.submit-button');
-
-function editProfile() {
-  let name = profile.querySelector('.profile__name-text');
-  let proffesion = profile.querySelector('.profile__proffession');
-  let fieldName = popup.querySelector('.popup__field_assign_name');
-  let fieldProffesion = popup.querySelector('.popup__field_assign_proffesion');
-
+function editProfile(event) {
+  event.preventDefault();
   name.textContent = fieldName.value;
   proffesion.textContent = fieldProffesion.value;
+  closeEditProfile();
 }
 
-submitButtonProfile.addEventListener('click', editProfile);
-submitButtonProfile.addEventListener('click', closeEditProfile);
+editButton.addEventListener('click', openEditProfile);
+escButton.addEventListener('click', closeEditProfile);
+submitButtonProfile.addEventListener('submit', editProfile);
+
