c1b1ee6c6f19b287d4871b45d1d739c1a7a6619d
Vladimir Rodionov <vsrodionov94@yandex.ru>
2020-07-19 20:54:27 +0700

    Восстановил

--- js/index.js
+++ js/index.js
@@ -4,18 +4,23 @@ const name = profile.querySelector('.profile__name-text');
 const profession = profile.querySelector('.profile__profession');
 const addButton = profile.querySelector('.profile__add-button')
 
-const modalEdit = document.querySelector('.modal_form_edit');
+const modalEdit = document.querySelector('.modal_assign_form-eidt');
 const escButtonModalEdit = modalEdit.querySelector('.modal__esc-button');
 const fieldName = modalEdit.querySelector('.modal__field_assign_name');
 const fieldprofession = modalEdit.querySelector('.modal__field_assign_profession');
 const formProfile = modalEdit.querySelector('.modal__container');
 
-const modalAdd = document.querySelector('.modal_form_add');
+const modalAdd = document.querySelector('.modal_assign_form-add');
 const escButtonModalAdd = modalAdd.querySelector('.modal__esc-button');
 const fieldTitle = modalAdd.querySelector('.modal__field_assign_title');
 const fieldLink = modalAdd.querySelector('.modal__field_assign_link');
 const formAdd = modalAdd.querySelector('.modal__container');
-const photo = document.querySelector('.photo');
+const photos = document.querySelector('.photos');
+const photo = photos.querySelector('.photo');
+
+const modalAlbum = document.querySelector('.modal_assign_album');
+const imageAlbum = modalAlbum.querySelector('.modal__large-photo');
+
 
 const initialCards = [
   {
@@ -62,7 +67,19 @@ function newInitialCards(event) {
   event.preventDefault();
   initialCards.unshift({name: fieldTitle.value, link: fieldLink.value});
   toggleModal(modalAdd);
-  console.log(initialCards);
+  addNewPhoto();
+}
+
+function addNewPhoto() {
+  photo.insertAdjacentHTML('afterbegin', `
+         <div class="photo__item">
+            <button class="photo__delete-button" type="button"></button>
+            <img class="photo__image" src="${initialCards[0].link}" alt="${initialCards[0].name}">
+            <div class="photo__caption">
+              <h2 class="photo__text">${initialCards[0].name}</h2>
+              <button class="photo__like-button" type="button"></button>
+            </div>
+         </div>`)
 }
 
 function startPhoto() {
@@ -77,9 +94,46 @@ function startPhoto() {
             </div>
          </div>`
   }
+  like();
+  deleteCard();
+  openPhoto();
 }
 
+function like() {
+  photo.onclick = function (event) {
+    if (!event.target.classList.contains('photo__like-button')) return;
+    let likeButton = event.target.closest('.photo__like-button');
+    likeButton.classList.toggle('photo__like-button_active');
+    console.log(likeButton)
+  };
+}
 
+function deleteCard() {
+  photos.onclick = function(event) {
+    if (event.target.className != 'photo__delete-button') return;
+    let photoCard = event.target.closest('.photo__item');
+    photoCard.remove();
+  }
+}
+
+function openPhoto() {
+  document.onclick = function(event) {
+    if (event.target.className != 'photo__image') return;
+    let openedPhoto = event.target.closest('.photo__image');
+    let srcImage = openedPhoto.getAttribute('src');
+    imageAlbum.innerHTML = `
+    <img class="modal__image" src="${srcImage}">
+    <button class="modal__esc-button" type="button" aria-label="Закрыть"></button>`
+    toggleModal(modalAlbum);
+    closeModalAlbum();
+  }
+}
+
+function closeModalAlbum() {
+  modalAlbum.onclick = function(event) {
+    if (event.target.className != 'modal__esc-button') return;
+    modalAlbum.classList.remove('modal_opened');
+}}
 
 window.onload = startPhoto();
 
@@ -90,3 +144,4 @@ formProfile.addEventListener('submit', submitModalEdit);
 addButton.addEventListener('click', function(){toggleModal(modalAdd)});
 escButtonModalAdd.addEventListener('click', function(){toggleModal(modalAdd)});
 formAdd.addEventListener('submit', newInitialCards);
+
