68564e58429b9634c983a3546199b472878b4775
Vladimir Rodionov <vsrodionov94@yandex.ru>
2020-07-19 19:28:54 +0700

    добавил новый попап

--- js/index.js
+++ js/index.js
@@ -1,35 +1,38 @@
 const profile = document.querySelector('.profile');
 const editButton = profile.querySelector('.profile__edit-button');
 const name = profile.querySelector('.profile__name-text');
-const proffesion = profile.querySelector('.profile__proffession');
+const profession = profile.querySelector('.profile__profession');
+const addButton = profile.querySelector('.profile__add-button')
 
 const modalEdit = document.querySelector('.modal_form_edit');
-const escButton = modalEdit.querySelector('.modal__esc-button');
+const escButtonModalEdit = modalEdit.querySelector('.modal__esc-button');
 const fieldName = modalEdit.querySelector('.modal__field_assign_name');
-const fieldProffesion = modalEdit.querySelector('.modal__field_assign_proffesion');
+const fieldprofession = modalEdit.querySelector('.modal__field_assign_profession');
 const formProfile = modalEdit.querySelector('.modal__container');
 
+const modalAdd = document.querySelector('.modal_form_add');
+const escButtonModalAdd = modalAdd.querySelector('.modal__esc-button');
+const fieldTitle = modalAdd.querySelector('.modal__field_assign_title');
+const fieldLink = modalAdd.querySelector('.modal__field_assign_link');
+
+
 function toggleModal(modalName) {
-  if (modalName.classList.contains('modal_opened')){
-    modalName.classList.remove('modal_opened');
-  }
   modalName.classList.toggle('modal_opened');
   fieldName.value = name.textContent;
-  fieldProffesion.value = proffesion.textContent;
+  fieldprofession.value = profession.textContent;
 }
-//
-// function closeEditProfile() {
-//   modal.classList.remove('modal_opened');
-// }
 
-function editProfile(event) {
+function submitModalEdit(event){
   event.preventDefault();
   name.textContent = fieldName.value;
-  proffesion.textContent = fieldProffesion.value;
-  closeEditProfile();
+  profession.textContent = fieldprofession.value;
+  toggleModal(modalEdit);
 }
 
-editButton.addEventListener('click', toggleModal(modalEdit));
-// escButton.addEventListener('click', closeEditProfile);
-formProfile.addEventListener('submit', editProfile);
 
+editButton.addEventListener('click', function(){toggleModal(modalEdit)});
+escButtonModalEdit.addEventListener('click', function(){toggleModal(modalEdit)});
+formProfile.addEventListener('submit', submitModalEdit);
+
+addButton.addEventListener('click', function(){toggleModal(modalAdd)});
+escButtonModalAdd.addEventListener('click', function(){toggleModal(modalAdd)});
