23253e9c051a4caeac83b1f9af2c89ba1da2ab30
Vladimir Rodionov <vsrodionov94@yandex.ru>
2020-09-19 22:42:31 +0700

    Все поправлено

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ export class Card {
       link,
       likes,
       id,
-      ownerId
+      owner
     },
     templateSelector,
     handleCardClick,
@@ -18,7 +18,7 @@ export class Card {
     this._link = link;
     this._likes = likes;
     this._id = id;
-    this._ownerId = ownerId;
+    this._owner = owner;
     this._templateSelector = templateSelector;
     this._handleCardClick = handleCardClick;
     this._handleLikeClick = handleLikeClick;
@@ -70,7 +70,7 @@ export class Card {
     this._photoCard = null;
   }
 
-  generateCard() {
+  generateCard(user) {
     this._photoCardElement = this._getTemplate();
     this._photoCardElementImage = this._photoCardElement.querySelector('.photo__image')
     this._photoCardElementImage.setAttribute('src', this._link);
@@ -82,21 +82,20 @@ export class Card {
     this._deleteButton = this._photoCardElement.querySelector('.photo__delete-button');
     this.howManylikes(this._likes.length);
     //скрываем кнопки удалить и ставим лайки
-    this._api.getUserData().then(data=>{
-      if (this._ownerId !== data._id) {
-        this._deleteButton.classList.add('card__delete-button_state_hidden');
-        this._deleteButton.classList.remove('card__delete-button_state_visible');
-      } else {
-        this._deleteButton.classList.add('card__delete-button_state_visible');
-        this._deleteButton.classList.remove('card__delete-button_state_hidden');
-      }
-      if (this._isLiked(data._id)) {
-        this._likeButton.classList.add('photo__like-button_active');
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    console.log(`Мой:${user._id}, Владелец: ${this._owner._id}`)
+    if (this._owner._id === user._id) {
+      console.log('Моё')
+      this._deleteButton.classList.add('card__delete-button_state_visible');
+      this._deleteButton.classList.remove('card__delete-button_state_hidden');
+
+    } else {
+      this._deleteButton.classList.add('card__delete-button_state_hidden');
+      this._deleteButton.classList.remove('card__delete-button_state_visible');
+    }
+    if (this._isLiked(user._id)) {
+      this._likeButton.classList.add('photo__like-button_active');
+    }
+
     this._photoCard = this._photoCardElement.querySelector('.photo__item');
     this._setEventListeners();
     return this._photoCardElement
--- src/pages/index.js
+++ src/pages/index.js
@@ -24,132 +24,6 @@ import { Api } from '../components/Api.js';
 import { Popup } from './../components/Popup.js';
 import { renderLoading } from './../utils/utils.js';
 
-
-
-//функции
-
-const handleSubmitAddNewCard = (inputValues, popup, form, section) => {
-  renderLoading(true, popup);
-  api
-  .addCard(inputValues)
-  .then((data) => {
-    createCard(data, section);
-    form.close();
-  })
-  .catch((err) => {
-    console.log(err); // выведем ошибку в консоль
-  })
-  .finally(()=>{
-    renderLoading(false, popup);
-  })
-}
-
-
-const handleCardClick = (item) => {
-  popupWithImage.open(item.link, item.name);
-}
-
-const handleLikeClick = (el, id, card) => {
-  if (el.classList.contains('photo__like-button_active')) {
-    api
-    .removeLike(id)
-    .then((data) => {
-      el.classList.remove('photo__like-button_active');
-      card.howManylikes(data.likes.length);
-    })
-    .catch((err) => {
-      console.log(err); // выведем ошибку в консоль
-    });
-  } else {
-
-    api
-    .putLike(id)
-    .then(data => {
-      el.classList.add('photo__like-button_active');
-      card.howManylikes(data.likes.length);
-    })
-    .catch((err) => {
-      console.log(err); // выведем ошибку в консоль
-    });
-  }
-}
-
-const handleConfirmDeleteCard = (id, el) => {
-  formConfirmDelete.addEventListener('submit', evt => {
-    evt.preventDefault();
-      api
-      .removeCard(id)
-      .then(()=>{
-        el.removeCard();
-        popupWithConfirmDelete.close();
-      })
-      .catch((err) => {
-        console.log(err); // выведем ошибку в консоль
-      });
-
-  })
-}
-
-const handleDeleteClick = (id, el) => {
-  popupWithConfirmDelete.open();
-  handleConfirmDeleteCard(id, el);
-}
-
-
-const fillPopupEdit = (name, profession) => {
-  fieldName.value = name;
-  fieldProfession.value = profession;
-}
-
-const createCard = (item, section, isArr) => {
-  const card = new Card(
-  item,
-  '#photo-item',
-  handleCardClick,
-  handleLikeClick,
-  handleDeleteClick,
-  handleConfirmDeleteCard,
-  api);
-
-  const cardElement = card.generateCard();
-  section.addItem(cardElement, isArr)
-}
-
-const handlerSubmitEditForm = (inputValues, popup, form) => {
-  popup.querySelector('.modal__submit-button').innerText = 'Сохранение...';
-    api
-    .setUserData(inputValues)
-    .then((data) => {
-      userInfo.setUserInfo(data.name, data.about);
-      form.close();
-    })
-    .catch((err) => {
-      console.log(err); // выведем ошибку в консоль
-    })
-    .finally(()=>{
-      popup.querySelector('.modal__submit-button').textContent = 'Сохранить';
-
-    })
-
-}
-
-const handlerSubmitAvatarForm = (inputValues, popup, form) => {
-  popup.querySelector('.modal__submit-button').innerText = 'Сохранение...';
-    api
-    .setUserAvatar(inputValues)
-    .then((data) => {
-      avatar.setAttribute('src', data.avatar);
-      form.close();
-    })
-    .catch((err) => {
-      console.log(err); // выведем ошибку в консоль
-    })
-    .finally(()=>{
-      popup.querySelector('.modal__submit-button').textContent = 'Сохранить';
-
-    })
-
-}
 // Экземпляр класса для Api
 const api = new Api({
   url: 'https://mesto.nomoreparties.co/v1/cohort-15',
@@ -157,114 +31,223 @@ const api = new Api({
   authorization: '87805956-615a-41b3-9626-fd0494106fb1',
   "Content-type": "application/json"
   }
-})
-
-// Экземпляр класса информации о пользователе
-const userInfo = new UserInfo(
-  '.profile__name-text',
-  '.profile__profession'
-);
-
-// создаем экземпляр класса для валидации формы редактирования профиля
-const validFormEdit = new FormValidator(formData, formProfile);
-validFormEdit.enableValidation();
-// создаем экземпляр класса для валидации формы добавления
-const validFormAdd = new FormValidator(formData, formAdd);
-validFormAdd.enableValidation();
-
-const validFormAvatar = new FormValidator(formData, formEditAvatar);
-validFormAvatar.enableValidation();
-
-
-// создаем экземпляры для попапов и вешаем слушателя
-
-
-
+});
 
 Promise.all([     //в Promise.all передаем массив промисов которые нужно выполнить
   api.getUserData(), // Получаем данные пользователя
   api.getInitialCards() // Получаем карточки с сервера
 ])
-.then((values)=>{    //попадаем сюда когда оба промиса будут выполнены
-  const [userData, initialCards] = values;
-  //Начальный рендер карточек
-  const cardSection = new Section({
-    items: initialCards.map((item) => {
-      return {
+.then((values)=>{
+//функции
+
+  const handleSubmitAddNewCard = (inputValues, popup, form) => {
+    renderLoading(true, popup);
+    api
+    .addCard(inputValues)
+    .then((data) => {
+      createCard(data);
+      form.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(()=>{
+      renderLoading(false, popup);
+    })
+  }
+
+  const handleCardClick = (item) => {
+    popupWithImage.open(item.link, item.name);
+  }
+
+  const handleLikeClick = (el, id, card) => {
+    if (el.classList.contains('photo__like-button_active')) {
+      api
+      .removeLike(id)
+      .then((data) => {
+        el.classList.remove('photo__like-button_active');
+        card.howManylikes(data.likes.length);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    } else {
+
+      api
+      .putLike(id)
+      .then(data => {
+        el.classList.add('photo__like-button_active');
+        card.howManylikes(data.likes.length);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  }
+
+  const handleConfirmDeleteCard = (id, el) => {
+    formConfirmDelete.addEventListener('submit', evt => {
+      evt.preventDefault();
+        api
+        .removeCard(id)
+        .then(()=>{
+          el.removeCard();
+          popupWithConfirmDelete.close();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+    })
+  }
+
+  const handleDeleteClick = (id, el) => {
+    popupWithConfirmDelete.open();
+    handleConfirmDeleteCard(id, el);
+  }
+
+  const fillPopupEdit = (name, profession) => {
+    fieldName.value = name;
+    fieldProfession.value = profession;
+  }
+
+  const createCard = (item, isArr) => {
+    const card = new Card(
+      {
         name: item.name,
         link: item.link,
         likes: item.likes,
         id: item._id,
-        ownerId: item.owner._id,
-      }
-    }),
-    renderer: createCard},
-    photoContainerSelector);
-    cardSection.renderItems();
+        owner: item.owner,
+      },
+    '#photo-item',
+    handleCardClick,
+    handleLikeClick,
+    handleDeleteClick,
+    handleConfirmDeleteCard,
+    api);
 
-  // начальная установка имени и профессии
-  userInfo.setUserInfo(userData.name, userData.about);
-  avatar.setAttribute('src', userData.avatar);
+    const cardElement = card.generateCard(userData);
+    cardSection.addItem(cardElement, isArr);
+  }
 
-  const popupWithFormEdit = new PopupWithForm(
-    '.modal_assign_form-eidt',
-    handlerSubmitEditForm,
-    api,
-    cardSection
+  const handlerSubmitEditForm = (inputValues, popup, form) => {
+    renderLoading(true, popup);
+      api
+      .setUserData(inputValues)
+      .then((data) => {
+        userInfo.setUserInfo(data.name, data.about);
+        form.close();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(()=>{
+        renderLoading(false, popup);
+      })
+  }
+
+  const handlerSubmitAvatarForm = (inputValues, popup, form) => {
+    renderLoading(true, popup);
+      api
+      .setUserAvatar(inputValues)
+      .then((data) => {
+        avatar.setAttribute('src', data.avatar);
+        form.close();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(()=>{
+        renderLoading(true, popup);
+      })
+  }
+
+  // Экземпляр класса информации о пользователе
+  const userInfo = new UserInfo(
+    '.profile__name-text',
+    '.profile__profession'
   );
-  popupWithFormEdit.setEventListeners();
 
-  const popupWithFormAdd = new PopupWithForm(
-    '.modal_assign_form-add',
-    handleSubmitAddNewCard,
-    api,
-    cardSection
-  )
-  popupWithFormAdd.setEventListeners();
+  // создаем экземпляр класса для валидации формы редактирования профиля
+  const validFormEdit = new FormValidator(formData, formProfile);
+  validFormEdit.enableValidation();
+  // создаем экземпляр класса для валидации формы добавления
+  const validFormAdd = new FormValidator(formData, formAdd);
+  validFormAdd.enableValidation();
 
-  const popupWithImage = new PopupWithImage(
-    '.modal_assign_album',
-    '.modal__image',
-    '.modal__caption'
-  );
-  popupWithImage.setEventListeners();
+  const validFormAvatar = new FormValidator(formData, formEditAvatar);
+  validFormAvatar.enableValidation();
+    //попадаем сюда когда оба промиса будут выполнены
+    const [userData, initialCards] = values;
+    //Начальный рендер карточек
+    const cardSection = new Section({
+      items: initialCards.map((item) => {
+        return {
+          name: item.name,
+          link: item.link,
+          likes: item.likes,
+          id: item._id,
+          owner: item.owner,
+        }
+      }),
+      renderer: createCard},
+      photoContainerSelector);
+      cardSection.renderItems();
 
-  const popupWithFormAvatar = new PopupWithForm(
-    '.modal_assign_form-avatar',
-    handlerSubmitAvatarForm,
-    api
-  )
-  popupWithFormAvatar.setEventListeners();
+    // начальная установка имени и профессии
+    userInfo.setUserInfo(userData.name, userData.about);
+    avatar.setAttribute('src', userData.avatar);
+  // создаем экземпляры для попапов и вешаем слушателя
+    const popupWithFormEdit = new PopupWithForm(
+      '.modal_assign_form-eidt',
+      handlerSubmitEditForm,
+      api,
+      cardSection
+    );
+    popupWithFormEdit.setEventListeners();
 
-  const popupWithConfirmDelete = new Popup('.modal_assign_confirm-delete');
-  popupWithConfirmDelete.setEventListeners();
+    const popupWithFormAdd = new PopupWithForm(
+      '.modal_assign_form-add',
+      handleSubmitAddNewCard,
+      api,
+      cardSection
+    )
+    popupWithFormAdd.setEventListeners();
 
-  // вешаем слушаетели на кнопки
-  editButton.addEventListener('click', function() {
-    fillPopupEdit(userInfo.getUserInfo().name, userInfo.getUserInfo().about);
-    popupWithFormEdit.open();
-    validFormEdit.activateButton();
-  });
+    const popupWithImage = new PopupWithImage(
+      '.modal_assign_album',
+      '.modal__image',
+      '.modal__caption'
+    );
+    popupWithImage.setEventListeners();
 
-  addButton.addEventListener('click', function() {
-    popupWithFormAdd.open();
-    validFormAdd.deactivateButton();
-  });
+    const popupWithFormAvatar = new PopupWithForm(
+      '.modal_assign_form-avatar',
+      handlerSubmitAvatarForm,
+      api
+    )
+    popupWithFormAvatar.setEventListeners();
 
-  avatarButton.addEventListener('click', function() {
-    popupWithFormAvatar.open();
-    validFormAvatar.deactivateButton();
-  })
+    const popupWithConfirmDelete = new Popup('.modal_assign_confirm-delete');
+    popupWithConfirmDelete.setEventListeners();
 
+    // вешаем слушаетели на кнопки
+    editButton.addEventListener('click', function() {
+      fillPopupEdit(userInfo.getUserInfo().name, userInfo.getUserInfo().about);
+      popupWithFormEdit.open();
+      validFormEdit.activateButton();
+    });
 
+    addButton.addEventListener('click', function() {
+      popupWithFormAdd.open();
+      validFormAdd.deactivateButton();
+    });
 
-
-
-
-
-
-
-
+    avatarButton.addEventListener('click', function() {
+      popupWithFormAvatar.open();
+      validFormAvatar.deactivateButton();
+    })
 })
 .catch((err)=>{
   console.log(err);
